deb7ada84a43b7f785d318c7bbfc45176564c818
Itai Tivoni <itai@ddaylabs.com>
2021-01-30 08:00:25 +0200

    finished with lesson #21

--- djblog/settings.py
+++ djblog/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'musicblog',
     'members',
+    'ckeditor',
 ]
 
 MIDDLEWARE = [
--- musicblog/models.py
+++ musicblog/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.urls import reverse
 from datetime import datetime, date
+from ckeditor.fields import RichTextField
 
 
 class Category(models.Model):
@@ -21,7 +22,8 @@ class Post(models.Model):
     title = models.CharField(max_length=255)
     title_tag = models.CharField(max_length=255)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
-    body = models.TextField()
+    body = RichTextField(blank=True, null=True)
+    # body = models.TextField()
     post_date = models.DateField(auto_now_add=True)
     categories = models.CharField(max_length=255, default='music')
     likes = models.ManyToManyField(User, related_name='music_blog')
