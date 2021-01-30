96346c795e5735ae160da736e75758ff51f2cc83
Itai Tivoni <itai@ddaylabs.com>
2021-01-30 21:28:23 +0200

    finished lesson 32

--- musicblog/models.py
+++ musicblog/models.py
@@ -30,6 +30,10 @@ class Profile(models.Model):
     def __str__(self):
         return str(self.user)
 
+    def get_absolute_url(self):
+
+        return reverse('home')
+
 
 class Post(models.Model):
 
