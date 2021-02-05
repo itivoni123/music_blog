c3898664ca749295afc1e2f646d473c1f413461d
Itai Tivoni <itai@ddaylabs.com>
2021-02-05 20:45:36 +0200

    replaced storage files

--- djblog/settings.py
+++ djblog/settings.py
@@ -136,7 +136,7 @@ STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 
 LOGIN_REDIRECT_URL = 'home'
 LOGOUT_REDIRECT_URL = 'home'
